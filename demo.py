92f9f64e211572ef9f090b8235f4eb8c1cadf176
Ty Andrews <ty.elgin.andrews@gmail.com>
2022-11-17 13:45:17 -0800

    added optional postiional arg

---
+++ demo.py
@@ -0,0 +1,17 @@
+# author: Tiffany Timbers
+# date: 2020-01-15
+
+"""This script prints out docopt args.
+Usage: demo.py <arg1> --arg2=<arg2> [--arg3=<arg3>] [<new_arg>]
+Options:
+<arg1>             Takes any value (this is a required positional argument)
+--arg2=<arg2>     Takes any value (this is a required option)
+[--arg3=<arg3>]   Takes any value (this is an optional option)
+[<new_arg>]       Takes any value, optional positional.
+"""
+
+from docopt import docopt
+
+opt = docopt(__doc__)
+print(opt)
+print(type(opt))
